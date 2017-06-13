702215b69271df65cfbdd0be80d9fb24fe76c02d
DK\DTAT <dtat@sitecore.net>
2017-06-13 13:07:00 +0300

    Patch commit

---
+++ src/Sitecore.Support.100371.100542/Cintel/Endpoint/Plumbing/InitializeRoutes.cs
@@ -0,0 +1,29 @@
+﻿using Sitecore.Pipelines;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace Sitecore.Support.Cintel.Endpoint.Plumbing
+{
+    public class InitializeRoutes : Sitecore.Cintel.Endpoint.Plumbing.InitializeRoutes
+    {
+        public override void Process(PipelineArgs args)
+        {
+            RouteTable.Routes.MapHttpRoute("cintel_aggregate_views_support", "sitecore/api/ao/v1/aggregates/{viewId}", new
+            {
+                controller = "AggregateViewSupport",
+                action = "Get"
+            });
+            RouteTable.Routes.MapHttpRoute("cintel_contact_intel_views_support",
+              "sitecore/api/ao/v1/contacts/{contactId}/intel/{viewId}/{id}", new
+              {
+                  controller = "IntelSupport",
+                  id = RouteParameter.Optional
+              });
+            RegisterRoutes(RouteTable.Routes, args);
+        }
+    }
+}
---
+++ src/Sitecore.Support.100371.100542/ExperienceAnalytics/Api/Pipelines/Initialize/WebApiInitializer.cs
@@ -0,0 +1,42 @@
+﻿namespace Sitecore.Support.ExperienceAnalytics.Api.Pipelines.Initialize
+{
+    using Sitecore.ExperienceAnalytics.Api;
+    using Sitecore.ExperienceAnalytics.Core.Diagnostics;
+    using Sitecore.Pipelines;
+    using System;
+    using System.Web.Http;
+
+    public class WebApiInitializer
+    {
+        //private readonly ILogger logger;
+
+        //public WebApiInitializer(ILogger logger)
+        //{
+        //    this.logger = logger;
+        //}
+
+        public void Process(PipelineArgs args)
+        {
+            ApiContainer.GetLogger().Info("WebApiInitializer", this);
+            HttpRouteCollectionExtensions.MapHttpRoute(GlobalConfiguration.Configuration.Routes, "AnalyticsDataApiSupport", "sitecore/api/ao/aggregates/{site}/{segments}/{keys}", new
+            {
+                controller = "AnalyticsDataSupport",
+                action = "GetSupport"
+            }, new
+            {
+                keys = "^[^.]+$"
+            });
+
+            ApiContainer.Configuration.GetWebApiConfiguration().Configure(GlobalConfiguration.Configuration);
+
+            //logger.Info("WebApiInitializer", this);
+            //HttpRouteCollectionExtensions.MapHttpRoute(GlobalConfiguration.Configuration.Routes, "AnalyticsDataApiSupport", "sitecore/api/ao/aggregates/{site}/{segments}/{keys}", new
+            //{
+            //    controller = "AnalyticsDataSupport",
+            //    action = "GetSupport"
+            //}, new { keys = "^[^.]+$" });
+
+            //ApiContainer.Configuration.GetWebApiConfiguration(logger).Configure(GlobalConfiguration.Configuration);
+        }
+    }
+}
